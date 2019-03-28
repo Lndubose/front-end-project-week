96283628d367231eb669a3e77eb19cf294a3178c
lndubose <lndubose13@gmail.com>
2019-03-28 15:09:38 -0500

    Restructure folders and add prop-type dependency

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,55 +1,63 @@
 import React, { Component } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
-import { AppStyle, Navigation, Main } from './components/style/appStyle';
+import { AppStyle, Navigation, Main } from './components/GlobalStyle/appStyle';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-import NoteList from './components/functional/NoteList';
-import NoteForm from './components/functional/NoteForm';
-import ViewPage from './components/functional/ViewPage';
+import NoteList from './components/Notes/NoteList';
+import NoteForm from './components/Form/NoteForm';
+import ViewPage from './components/Notes/ViewPage';
 
 class App extends Component {
-  render() {
-    return (
-      <Route
-        render={({ location }) => (
-          <AppStyle>
-            <Navigation>
-              <h1>Lambda Notes</h1>
-              <Link to="/" className="link">
-                View Your Notes
-              </Link>
-              <Link to="/noteform/create" className="link">
-                + Create New Note
-              </Link>
-            </Navigation>
-            <Main>
-              <TransitionGroup className="transition-group">
-                <CSSTransition
-                  key={location.key}
-                  timeout={{ enter: 700, exit: 700 }}
-                  classNames="slide"
-                >
-                  <section className="route-section">
-                    <Switch location={location}>
-                      <Route exact path="/" component={NoteList} />
-                      <Route
-                        path="/noteform/:id?"
-                        render={props => <NoteForm {...props} />}
-                      />
-                      <Route
-                        path="/notes/:id"
-                        render={props => <ViewPage {...props} />}
-                      />
-                    </Switch>
-                  </section>
-                </CSSTransition>
-              </TransitionGroup>
-            </Main>
-          </AppStyle>
-        )}
-      />
-    );
-  }
+    render() {
+        return (
+            <Route
+                render={({ location }) => (
+                    <AppStyle>
+                        <Navigation>
+                            <h1>Lambda Notes</h1>
+                            <Link to="/" className="link">
+                                View Your Notes
+                            </Link>
+                            <Link to="/noteform/create" className="link">
+                                + Create New Note
+                            </Link>
+                        </Navigation>
+                        <Main>
+                            <TransitionGroup className="transition-group">
+                                <CSSTransition
+                                    key={location.key}
+                                    timeout={{ enter: 700, exit: 700 }}
+                                    classNames="slide"
+                                >
+                                    <section className="route-section">
+                                        <Switch location={location}>
+                                            <Route
+                                                exact
+                                                path="/"
+                                                component={NoteList}
+                                            />
+                                            <Route
+                                                path="/noteform/:id?"
+                                                render={props => (
+                                                    <NoteForm {...props} />
+                                                )}
+                                            />
+                                            <Route
+                                                path="/notes/:id"
+                                                render={props => (
+                                                    <ViewPage {...props} />
+                                                )}
+                                            />
+                                        </Switch>
+                                    </section>
+                                </CSSTransition>
+                            </TransitionGroup>
+                        </Main>
+                    </AppStyle>
+                )}
+            />
+        );
+    }
 }
 
 export default App;
---
+++ notes/src/components/Notes/NoteList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { getData } from '../../store/actions';
+import Note from './Note';
+
+import { NotesContainer, NoteAlign } from './noteStyle';
+
+class NoteList extends React.Component {
+    componentDidMount() {
+        this.props.getData();
+    }
+
+    render() {
+        return (
+            <NotesContainer>
+                <h2>Your Notes:</h2>
+                <NoteAlign>
+                    {this.props.notes.map(note => (
+                        <Link to={`/notes/${note._id}`} key={note._id}>
+                            <Note note={note} />
+                        </Link>
+                    ))}
+                </NoteAlign>
+            </NotesContainer>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        notes: state.notes,
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    { getData }
+)(NoteList);
